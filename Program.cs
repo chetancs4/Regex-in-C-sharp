49cbf3e6b9b4f2511b1345189b8d2eef4d7cdfc9
Chetan <chetansadvelkar4@gmail.com>
2023-05-24 21:37:51 +0530

    [Chetan] Add: This script is used to enter valid First Name.

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿namespace RegexProgram
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Regex Program");
+            RegClass r = new RegClass();
+            r.Reg();
+        }
+    }
+}
---
+++ RegClass.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace RegexProgram
+{
+    internal class RegClass
+    {
+        public void Reg() 
+        {
+            string name = @"^[A-Z]{1}[a-z]{2,}$";
+            Regex rg = new Regex(name);
+
+            Console.WriteLine("Enter First Name: ");
+            string FirstName = Console.ReadLine();
+            if (rg.IsMatch(FirstName))
+            {
+                Console.WriteLine("First Name: " + FirstName);
+            }
+            else
+            {
+                Console.WriteLine("invalid First Name");
+            }
+        }
+    }
+}

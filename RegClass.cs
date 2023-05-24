6cb5f5772afe695eb45b648799c8ffa31b347884
Chetan <chetansadvelkar4@gmail.com>
2023-05-25 02:25:56 +0530

    [Chetan] Add: This script is used to add valid Password.

--- RegClass.cs
+++ RegClass.cs
@@ -12,15 +12,16 @@ namespace RegexProgram
             Regex re = new Regex(email);
             string phone = @"\d{2}\s\d{10}$";
             Regex rp = new Regex(phone);
-
-            Console.Write("Enter First Name: ");
-            string firstName = Console.ReadLine();
-            Console.Write("Enter Last Name: ");
-            string lastName = Console.ReadLine();
-           
+            String pass = @"^.{0,7}|[^A-Z]*|[^0-9]*|[^.]*\W{2,}[^.]*|[^\W]$";
+            //@"^(?=.[A-Z])(?=.[0-9])(?=.\W).{8,}$";
+            Regex rpass = new Regex(pass);
 
             while (true)
             {
+                Console.Write("Enter First Name: ");
+                string firstName = Console.ReadLine();
+                Console.Write("Enter Last Name: ");
+                string lastName = Console.ReadLine();
                 if (rg.IsMatch(firstName) && rg.IsMatch(lastName))
                 {
                     Console.Write("Enter Email Address: ");
@@ -31,11 +32,23 @@ namespace RegexProgram
                         string PhoneNumber = Console.ReadLine();
                         if (rp.IsMatch(PhoneNumber))
                         {
-                            Console.WriteLine("User Details: ");
-                            Console.WriteLine("First Name: " + firstName);
-                            Console.WriteLine("Last Name: " + lastName);
-                            Console.WriteLine("Email ID: " + emailid);
-                            Console.WriteLine("Phone Number: " + PhoneNumber);
+                            Console.Write("Enter the Password : ");
+                            string password = Console.ReadLine();
+                            if (rpass.IsMatch(password))
+                            {
+                                Console.WriteLine();
+                                Console.WriteLine("User Details:- ");
+                                Console.WriteLine("First Name: " + firstName);
+                                Console.WriteLine("Last Name: " + lastName);
+                                Console.WriteLine("Email ID: " + emailid);
+                                Console.WriteLine("Phone Number: " + PhoneNumber);
+                                Console.WriteLine("Password: " + password);
+                            }
+                            else
+                            {
+                                Console.WriteLine("invalid Password");
+                                break;
+                            }
                         }
                         else
                         {
@@ -54,6 +67,7 @@ namespace RegexProgram
                     Console.WriteLine("Invalid Name");
                     break;
                 }
+                break;
             }
         }
     }
